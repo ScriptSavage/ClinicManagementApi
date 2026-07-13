e523acfaf947337457382d3d50d24b52c045d7b8
MAX <maksymiliangaleziowski@gmail.com>
2026-07-13 17:57:02 +0200

    feat: added Doctor entity

---
+++ ClinicManagement/Infrastructure/Configuration/DoctorConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configuration;
+
+public class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
+{
+    public void Configure(EntityTypeBuilder<Doctor> builder)
+    {
+        builder.HasKey(e => e.DoctorId);
+        
+        
+        builder.Property(e=>e.FirstName)
+            .IsRequired()
+            .HasMaxLength(250);
+        
+        builder.Property(e=>e.LastName)
+            .IsRequired()
+            .HasMaxLength(250);
+        
+        builder.Property(e=>e.PWZ)
+            .IsRequired()
+            .HasMaxLength(20);
+        
+        builder.HasIndex(e=>e.PWZ)
+            .IsUnique();
+        
+        
+        builder.HasOne(e=>e.User)
+            .WithOne(e => e.Doctor)
+            .HasForeignKey<Doctor>(e => e.UserId);
+        
+    }
+}
---
+++ ClinicManagement/Infrastructure/Entities/Doctor.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Entities;
+
+public class Doctor
+{
+    public Guid DoctorId { get; set; }
+    public string PWZ { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+
+
+    public ApplicationUser User { get; set; } = null!;
+    public Guid UserId { get; set; }
+
+}
