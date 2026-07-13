884f235d6bd3320f8761b271f39973445caaad52
MAX <maksymiliangaleziowski@gmail.com>
2026-07-13 17:40:48 +0200

    feat: create PatientRepository

---
+++ ClinicManagement/Infrastructure/Repositories/IPatientRepository.cs
@@ -0,0 +1,8 @@
+﻿using Infrastructure.Entities;
+
+namespace Infrastructure.Repositories;
+
+public interface IPatientRepository
+{
+    Task AddNewPatientAsync(Patient patient);
+}
---
+++ ClinicManagement/Infrastructure/Repositories/PatientRepository.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Context;
+using Infrastructure.Entities;
+
+namespace Infrastructure.Repositories;
+
+public class PatientRepository : IPatientRepository
+{
+    private readonly DatabaseContext _context;
+
+    public PatientRepository(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddNewPatientAsync(Patient patient)
+    {
+        await _context.Patients.AddAsync(patient);
+    }
+}
