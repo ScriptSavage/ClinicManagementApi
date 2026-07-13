435ba7edff9da07480fd9718fdaf615939e9d933
MAX <maksymiliangaleziowski@gmail.com>
2026-07-13 17:41:31 +0200

    feat: added PatientConfiguration

---
+++ ClinicManagement/Infrastructure/Configuration/PatientConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configuration;
+
+public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+{
+    public void Configure(EntityTypeBuilder<Patient> builder)
+    {
+        builder.HasKey(e => e.PatientId);
+        
+        builder.Property(e=>e.FirstName)
+            .HasMaxLength(250)
+            .IsRequired();
+        
+        builder.Property(e=>e.LastName)
+            .HasMaxLength(250)
+            .IsRequired();
+        
+        builder.Property(e=>e.Pesel)
+            .HasMaxLength(11)
+            .IsRequired();
+        
+        builder.HasIndex(e=>e.Pesel)
+            .IsUnique();
+        
+        builder.Property(e=>e.DateOfBirth)
+            .IsRequired();
+
+        builder.HasOne(e => e.ApplicationUser)
+            .WithOne(e => e.Patient)
+            .HasForeignKey<Patient>(e => e.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        builder.HasIndex(e=>e.UserId)
+            .IsUnique();
+
+    }
+}
