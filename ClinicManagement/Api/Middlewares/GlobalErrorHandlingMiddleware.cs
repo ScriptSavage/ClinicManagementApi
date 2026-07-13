ce5347a6668c2a419509fa6a1c7cdb4b70b6e62e
MAX <maksymiliangaleziowski@gmail.com>
2026-07-13 17:56:35 +0200

    feat: added GlobalErrorHandlingMiddleware

---
+++ ClinicManagement/Api/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Middlewares;
+
+public class GlobalErrorHandlingMiddleware :  IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next.Invoke(context);
+        }
+        catch(ValidationException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+        catch (Exception e)
+        {
+            context.Response.StatusCode = 500;
+        }
+    }
+}
