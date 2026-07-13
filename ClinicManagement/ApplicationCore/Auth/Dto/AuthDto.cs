26dbadc34ea79a8ee8dbefa400c3ec2933b9f97c
MAX <maksymiliangaleziowski@gmail.com>
2026-07-13 17:58:10 +0200

    feat: added AuthDto

---
+++ ClinicManagement/ApplicationCore/Auth/Dto/AuthDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ApplicationCore.Auth.Dto;
+
+public static class AuthDto
+{
+    public record RegisterNewPatient(
+        string FirstName,
+        string LastName,
+        string Pesel,
+        DateTime DateOfBirth,
+        string EmailAddress, 
+        string Password,
+        string ConfirmPassword,
+        string PhoneNumber);
+}
