5714ef091ac201cf0913562061569c9d84b986c0
MAX <maksymiliangaleziowski@gmail.com>
2026-07-13 17:42:20 +0200

    feat: added AuthController

---
+++ ClinicManagement/Api/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.Auth.Dto;
+using ApplicationCore.Auth.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public async Task<IActionResult> RegisterNewPatient([FromBody] AuthDto.RegisterNewPatient request)
+    {
+        await _authService.RegisterNewPatient(request);
+        return Ok(new { Message = "Registration Successful" });
+    }
+}
