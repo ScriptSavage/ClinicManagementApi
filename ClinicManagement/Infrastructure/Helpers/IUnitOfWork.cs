f0d7db8dbb9a7cb463758cb49aa504ba030c05fe
MAX <maksymiliangaleziowski@gmail.com>
2026-07-13 17:40:29 +0200

    feat: added UnitOfWork pattern

---
+++ ClinicManagement/Infrastructure/Helpers/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Infrastructure.Helpers;
+
+public interface IUnitOfWork
+{
+    Task SaveChangesAsync();
+    
+    Task<IDbContextTransaction> BeginTransactionAsync();
+    Task RollbackTransaction();
+    
+}
---
+++ ClinicManagement/Infrastructure/Helpers/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Infrastructure.Helpers;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DatabaseContext _context;
+
+    public UnitOfWork(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+       await _context.SaveChangesAsync();
+    }
+
+    public async Task<IDbContextTransaction> BeginTransactionAsync()
+    {
+        return await _context.Database.BeginTransactionAsync();
+    }
+
+    public async Task RollbackTransaction()
+    {
+       await _context.Database.RollbackTransactionAsync();
+    }
+}
