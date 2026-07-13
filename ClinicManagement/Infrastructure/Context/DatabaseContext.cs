55f206584d268243d6fdbdd4751de2ab6a1b9158
MAX <maksymiliangaleziowski@gmail.com>
2026-07-13 17:57:38 +0200

    feat: added DatabaseContext

---
+++ ClinicManagement/Infrastructure/Context/DatabaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Configuration;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class DatabaseContext : IdentityDbContext<ApplicationUser,ApplicationRole,Guid>
+{
+    public DatabaseContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DatabaseContext()
+    {
+    }
+
+    public DbSet<ApplicationUser> Users { get; set; }
+    public DbSet<ApplicationRole> Roles { get; set; } 
+    public DbSet<Patient> Patients { get; set; } 
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new ApplicationUserConfiguration());
+        builder.ApplyConfiguration(new ApplicationRoleConfiguration());
+        builder.ApplyConfiguration(new PatientConfiguration());
+        builder.ApplyConfiguration(new DoctorConfiguration());
+        base.OnModelCreating(builder);
+        
+    }
+}
