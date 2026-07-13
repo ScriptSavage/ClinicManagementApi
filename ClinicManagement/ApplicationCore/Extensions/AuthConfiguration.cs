f016b299d798e3f6d41cfc6e96491867eb3eb9db
MAX <maksymiliangaleziowski@gmail.com>
2026-07-13 17:58:19 +0200

    commit

---
+++ ClinicManagement/ApplicationCore/Extensions/AuthConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Context;
+using Infrastructure.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApplicationCore.Extensions;
+
+public static class AuthConfiguration
+{
+    public static void ConfigureAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddIdentityCore<ApplicationUser>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireDigit = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+            .AddRoles<ApplicationRole>()
+            .AddEntityFrameworkStores<DatabaseContext>();
+    }
+}
