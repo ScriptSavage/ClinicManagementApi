0bfdb58f151e9ba84ef9e55ce4855c68f16a8c76
MAX <maksymiliangaleziowski@gmail.com>
2026-07-13 17:41:13 +0200

    feat: added Patient Entity

---
+++ ClinicManagement/Infrastructure/Entities/Patient.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Entities;
+
+public class Patient
+{
+    public Guid PatientId { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Pesel { get; set; } = null!;
+    public DateTime DateOfBirth { get; set; }
+
+
+    public ApplicationUser ApplicationUser { get; set; } = null!;
+    public Guid UserId { get; set; }
+}
