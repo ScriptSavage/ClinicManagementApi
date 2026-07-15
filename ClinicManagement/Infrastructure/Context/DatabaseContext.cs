2959f53a6a8017d1fe7e58dd529a2438e7edab8e
maksymilian <maksymiliangaleziowski@gmail.com>
2026-07-15 08:56:52 +0200

    feat: added missing Datasets and configurations

--- ClinicManagement/Infrastructure/Context/DatabaseContext.cs
+++ ClinicManagement/Infrastructure/Context/DatabaseContext.cs
@@ -16,8 +16,16 @@ public class DatabaseContext : IdentityDbContext<ApplicationUser,ApplicationRole
     }
 
     public DbSet<ApplicationUser> Users { get; set; }
-    public DbSet<ApplicationRole> Roles { get; set; } 
+    public DbSet<ApplicationRole> Roles { get; set; }
+    public DbSet<Specialization> Specializations { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Address> Addresses { get; set; }
     public DbSet<Patient> Patients { get; set; } 
+    public DbSet<Visit> Visits { get; set; } 
+    public DbSet<Prescription> Prescriptions { get; set; } 
+    public DbSet<Producer> Producers { get; set; } 
+    public DbSet<Medicine> Medicines { get; set; } 
+    public DbSet<MedicinePrescription> MedicinePrescription { get; set; } 
 
 
     protected override void OnModelCreating(ModelBuilder builder)
@@ -26,6 +34,13 @@ public class DatabaseContext : IdentityDbContext<ApplicationUser,ApplicationRole
         builder.ApplyConfiguration(new ApplicationRoleConfiguration());
         builder.ApplyConfiguration(new PatientConfiguration());
         builder.ApplyConfiguration(new DoctorConfiguration());
+        builder.ApplyConfiguration(new AddressConfiguration());
+        builder.ApplyConfiguration(new SpecializationConfiguration());
+        builder.ApplyConfiguration(new VisitConfiguration());
+        builder.ApplyConfiguration(new PrescriptionConfiguration());
+        builder.ApplyConfiguration(new ProducerConfiguration());
+        builder.ApplyConfiguration(new MedicineConfiguration());
+        builder.ApplyConfiguration(new MedicinePrescriptionConfiguration());
         base.OnModelCreating(builder);
         
     }
