8b582ee211a19f30afe7db4303979bd1de175a94
MAX <maksymiliangaleziowski@gmail.com>
2026-07-18 10:59:39 +0200

    feat: added RoleSeeder

---
+++ ClinicManagement/ApplicationCore/Seeders/RoleSeeder.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApplicationCore.Seeders;
+
+public static class RoleSeeder
+{
+    private static string[] _roles =
+    {
+        "Admin",
+        "Patient",
+        "Doctor",
+        "Employee"
+    };
+
+
+    public static async Task SeedRolesAsync(IServiceProvider services)
+    {
+        using var scope = services.CreateScope();
+
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+
+        foreach (var roleName in _roles)
+        {
+            if (await roleManager.RoleExistsAsync(roleName)) continue;
+
+            await roleManager.CreateAsync(new ApplicationRole
+            {
+                Name = roleName
+            });
+            
+        }
+    }
+
+}
