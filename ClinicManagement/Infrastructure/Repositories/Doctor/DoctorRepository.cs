cfd02b50b74e731499ace3d22729a8995c416ffa
MAX <maksymiliangaleziowski@gmail.com>
2026-07-19 13:02:29 +0200

    feat: added DoctorRepository

---
+++ ClinicManagement/Infrastructure/Repositories/Doctor/DoctorRepository.cs
@@ -0,0 +1,25 @@
+﻿using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Doctor;
+
+public class DoctorRepository : IDoctorRepository
+{
+    private readonly DatabaseContext _context;
+
+    public DoctorRepository(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddNewDoctorAsync(Entities.Doctor doctor)
+    {
+        await _context.Doctors.AddAsync(doctor);
+    }
+
+    public async Task<bool> DoesDoctorExistAsync(string Pwz)
+    {
+        return await _context.Doctors.AnyAsync(e=>e.PWZ == Pwz);
+        
+    }
+}
---
+++ ClinicManagement/Infrastructure/Repositories/Doctor/IDoctorRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Repositories.Doctor;
+
+public interface IDoctorRepository
+{
+    Task AddNewDoctorAsync(Entities.Doctor doctor);
+    
+    Task<bool> DoesDoctorExistAsync(string Pwz);
+}
