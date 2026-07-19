6a0438ce474b54a1601f60f8f350fa964d87b26d
MAX <maksymiliangaleziowski@gmail.com>
2026-07-19 13:01:23 +0200

    feat: added new exception type

--- ClinicManagement/Api/Middlewares/GlobalErrorHandlingMiddleware.cs
+++ ClinicManagement/Api/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using ApplicationCore.Exceptions;
+using FluentValidation;
 
 namespace Api.Middlewares;
 
@@ -10,13 +11,20 @@ public class GlobalErrorHandlingMiddleware : IMiddleware
         {
             await next.Invoke(context);
         }
-        catch(ValidationException ex)
+        catch (AlreadyExistsException e)
         {
-           context.Response.StatusCode =  StatusCodes.Status400BadRequest;
+            context.Response.StatusCode = StatusCodes.Status409Conflict;
+            await context.Response.WriteAsync(e.Message);
+        }
+        catch (ValidationException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            Console.WriteLine(ex.Message);
         }
         catch (Exception e)
         {
             context.Response.StatusCode = 500;
+            Console.WriteLine(e.Message);
         }
     }
 }
---
+++ ClinicManagement/ApplicationCore/Exceptions/AlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationCore.Exceptions;
+
+public class AlreadyExistsException : Exception
+{
+    public AlreadyExistsException()
+    {
+    }
+
+    public AlreadyExistsException(string? message) : base(message)
+    {
+    }
+}
