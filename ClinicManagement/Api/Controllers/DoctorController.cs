7c0c8859e323594e600b371d42e3ddb9908d1159
MAX <maksymiliangaleziowski@gmail.com>
2026-07-19 13:01:48 +0200

    feat: added DoctorController

---
+++ ClinicManagement/Api/Controllers/DoctorController.cs
@@ -0,0 +1,26 @@
+﻿using ApplicationCore.Doctor.Dto;
+using ApplicationCore.Doctor.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/doctors")]
+public class DoctorController : ControllerBase
+{
+    private readonly IDoctorService _doctorService;
+
+    public DoctorController(IDoctorService doctorService)
+    {
+        _doctorService = doctorService;
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Post([FromBody] DoctorDto.CreateDoctorDto request)
+    {
+        await _doctorService.AddNewDoctor(request);
+        return Ok(new { Message = "Doctor Added Successfully" });
+    }
+}
