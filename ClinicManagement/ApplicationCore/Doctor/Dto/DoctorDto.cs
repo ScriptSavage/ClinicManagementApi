4cbfbc314dfe99e84e51810aafe4270cbaacb388
MAX <maksymiliangaleziowski@gmail.com>
2026-07-19 13:04:00 +0200

    feat: added DoctorDto

---
+++ ClinicManagement/ApplicationCore/Doctor/Dto/DoctorDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationCore.Doctor.Dto;
+
+public static class DoctorDto
+{
+    public record CreateDoctorDto(
+        string Login,
+        string Password,
+        string Email,
+        string FirstName,
+        string LastName,
+        string Pwz);
+}
