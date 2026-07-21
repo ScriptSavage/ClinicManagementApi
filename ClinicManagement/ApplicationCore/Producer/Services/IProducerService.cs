334240d0f3f12db72dfb1688fcc82ebe49294352
MAX <maksymiliangaleziowski@gmail.com>
2026-07-21 20:20:50 +0200

    feat: added ProducerService

---
+++ ClinicManagement/ApplicationCore/Producer/Services/IProducerService.cs
@@ -0,0 +1,9 @@
+﻿
+using ApplicationCore.Producer.Dto;
+
+namespace ApplicationCore.Producer.Services;
+
+public interface IProducerService
+{
+    Task AddNewProducer(ProducerDto.NewProducer dto);
+}
---
+++ ClinicManagement/ApplicationCore/Producer/Services/ProducerService.cs
@@ -0,0 +1,38 @@
+﻿using ApplicationCore.Producer.Dto;
+using FluentValidation;
+using Infrastructure.Helpers;
+using Infrastructure.Repositories.Producer;
+
+namespace ApplicationCore.Producer.Services;
+
+public class ProducerService : IProducerService
+{
+    private readonly IProducerRepository _producerRepository;
+    private readonly IUnitOfWork  _unitOfWork;
+    private readonly IValidator<ProducerDto.NewProducer>  _validator;
+
+    public ProducerService(IProducerRepository producerRepository,
+        IUnitOfWork unitOfWork,
+        IValidator<ProducerDto.NewProducer> validator)
+    {
+        _producerRepository = producerRepository;
+        _unitOfWork = unitOfWork;
+        _validator = validator;
+    }
+
+    public async Task AddNewProducer(ProducerDto.NewProducer dto)
+    {
+        var validationResult = await _validator.ValidateAsync(dto);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
+
+        var newProducer = new Infrastructure.Entities.Producer
+        {
+            Name = dto.Name,
+        };
+        await _producerRepository.CreateAsync(newProducer);
+        await _unitOfWork.SaveChangesAsync();
+    }
+}
