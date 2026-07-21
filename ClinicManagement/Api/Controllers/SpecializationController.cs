95c603428ec8cca27f1e993ae928cedb8b97ed56
MAX <maksymiliangaleziowski@gmail.com>
2026-07-21 20:26:02 +0200

    feat: added SpecializationController

---
+++ ClinicManagement/Api/Controllers/SpecializationController.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationCore.Specialization.Dto;
+using ApplicationCore.Specialization.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/specializations")]
+public class SpecializationController : ControllerBase
+{
+    private readonly ISpecializationService _specializationService;
+
+    public SpecializationController(ISpecializationService specializationService)
+    {
+        _specializationService = specializationService;
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> AddNewSpecialization(SpecializationDto.AddNewSpecialization specializationDto)
+    {
+        await _specializationService.AddNewSpecialization(specializationDto);
+        return Ok(new
+        {
+            Message = "Specialization added successfully",
+        });
+    }
+    
+}
