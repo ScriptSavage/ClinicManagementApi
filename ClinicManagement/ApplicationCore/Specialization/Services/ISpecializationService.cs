526dbcb4f0ab5439c9ca65306a173d3e3749f580
MAX <maksymiliangaleziowski@gmail.com>
2026-07-21 20:19:45 +0200

    feat: added SpecializationService

---
+++ ClinicManagement/ApplicationCore/Specialization/Services/ISpecializationService.cs
@@ -0,0 +1,9 @@
+﻿using ApplicationCore.Specialization.Dto;
+
+namespace ApplicationCore.Specialization.Services;
+
+public interface ISpecializationService
+{
+    Task AddNewSpecialization(SpecializationDto.AddNewSpecialization dto);
+    
+}
---
+++ ClinicManagement/ApplicationCore/Specialization/Services/SpecializationService.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationCore.Specialization.Dto;
+using Infrastructure.Helpers;
+using Infrastructure.Repositories.Specialization;
+
+namespace ApplicationCore.Specialization.Services;
+
+public class SpecializationService : ISpecializationService
+{
+    private readonly ISpecializationRepository _specializationRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public SpecializationService(ISpecializationRepository specializationRepository, 
+        IUnitOfWork unitOfWork)
+    {
+        _specializationRepository = specializationRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task AddNewSpecialization(SpecializationDto.AddNewSpecialization dto)
+    {
+        var newSpecialization = new Infrastructure.Entities.Specialization
+        {
+            Name = dto.SpecializationName,
+            Description = dto.SpecializationDescription,
+        };
+
+        await _specializationRepository.CreateAsync(newSpecialization);
+        await _unitOfWork.SaveChangesAsync();
+    }
+}
