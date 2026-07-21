17888cf0bf970f066eefb7f7bdd6e50532f1cca1
MAX <maksymiliangaleziowski@gmail.com>
2026-07-21 20:20:08 +0200

    feat: added SpecializationRepository

---
+++ ClinicManagement/Infrastructure/Repositories/Specialization/ISpecializationRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Repositories.Specialization;
+
+public interface ISpecializationRepository : IRepository<Entities.Specialization>
+{
+    Task<IEnumerable<Entities.Specialization>> GetSpecializations(IEnumerable<Guid> ids);
+    
+    Task<Entities.Specialization> GetSpecialization(Guid id);
+}
---
+++ ClinicManagement/Infrastructure/Repositories/Specialization/SpecializationRepository.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Specialization;
+
+
+public class SpecializationRepository : ISpecializationRepository
+{
+    private readonly DatabaseContext _context;
+    
+
+    public SpecializationRepository(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateAsync(Entities.Specialization entity) => await _context.Specializations.AddAsync(entity);
+    
+    public void Delete(Entities.Specialization entity) => _context.Specializations.Remove(entity);
+    
+    public async Task<IEnumerable<Entities.Specialization>> GetSpecializations(IEnumerable<Guid> ids)
+    {
+        var data = await _context.Specializations
+            .Where(e => ids.Contains(e.SpecializationId))
+            .ToListAsync();
+        
+        return data;
+    }
+
+    public async Task<Entities.Specialization> GetSpecialization(Guid id) => await _context.Specializations
+        .FirstOrDefaultAsync(e => e.SpecializationId == id);
+}
