92555c5db4501ac66ca6c2684161b889c27f3cff
MAX <maksymiliangaleziowski@gmail.com>
2026-07-21 20:25:17 +0200

    feat: added ProducerController

---
+++ ClinicManagement/Api/Controllers/ProducerController.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationCore.Producer.Dto;
+using ApplicationCore.Producer.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/producers")]
+public class ProducerController : ControllerBase
+{
+    private readonly IProducerService _producerService;
+
+    public ProducerController(IProducerService producerService)
+    {
+        _producerService = producerService;
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Post([FromBody] ProducerDto.NewProducer producerDto)
+    {
+        await _producerService.AddNewProducer(producerDto);
+        return Ok(new
+        {
+            Message = "Producer created"
+        });
+    }
+}
