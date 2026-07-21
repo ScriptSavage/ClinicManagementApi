432a54100095e559f053d2e8b49820d7813b597b
MAX <maksymiliangaleziowski@gmail.com>
2026-07-21 20:25:02 +0200

    feat: added NewProducerValidator

---
+++ ClinicManagement/ApplicationCore/Producer/Validators/NewProducerValidator.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Producer.Dto;
+using FluentValidation;
+
+namespace ApplicationCore.Producer.Validators;
+
+public class NewProducerValidator :  AbstractValidator<ProducerDto.NewProducer>
+{
+    public NewProducerValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required")
+            .NotNull().WithMessage("Name is required")
+            .MinimumLength(2);
+    }
+}
