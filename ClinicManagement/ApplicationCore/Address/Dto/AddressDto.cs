60566b8ee586327a82b99d399376a0ca0cd357d3
MAX <maksymiliangaleziowski@gmail.com>
2026-07-21 20:21:05 +0200

    feat: added AddressDto

---
+++ ClinicManagement/ApplicationCore/Address/Dto/AddressDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationCore.Address.Dto;
+
+public static class AddressDto
+{
+    public record NewAddress(
+        string Street, 
+        string City, 
+        string State, 
+        string ZipCode);
+}
