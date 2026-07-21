6ea14b26148647c9f367c52d5eeea763fab94ecd
MAX <maksymiliangaleziowski@gmail.com>
2026-07-21 20:24:23 +0200

    feat: added ProducerRepository

---
+++ ClinicManagement/Infrastructure/Repositories/Producer/IProducerRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Infrastructure.Repositories.Producer;
+
+public interface IProducerRepository : IRepository<Entities.Producer>
+{
+    
+}
---
+++ ClinicManagement/Infrastructure/Repositories/Producer/ProducerRepository.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Context;
+
+namespace Infrastructure.Repositories.Producer;
+
+public class ProducerRepository : IProducerRepository
+{
+    private readonly DatabaseContext _context;
+
+    public ProducerRepository(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateAsync(Entities.Producer entity)
+    {
+        await _context.Producers.AddAsync(entity);
+    }
+
+    public void Delete(Entities.Producer entity)
+    {
+        _context.Producers.Remove(entity);
+    }
+}
