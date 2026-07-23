8982c2325fccaf5df8f42efe839c0088287abe32
maksymilian <maksymiliangaleziowski@gmail.com>
2026-07-23 16:48:09 +0200

    feat: added DoesNotExistsException

---
+++ ClinicManagement/ApplicationCore/Exceptions/DoesNotExistsException.cs
@@ -0,0 +1,12 @@
+namespace ApplicationCore.Exceptions;
+
+public class DoesNotExistsException : Exception
+{
+    public DoesNotExistsException()
+    {
+    }
+
+    public DoesNotExistsException(string? message) : base(message)
+    {
+    }
+}
