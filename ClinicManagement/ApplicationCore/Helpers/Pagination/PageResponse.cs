d6f972873ff43eb002e3991f7cfc381add399881
maksymilian <maksymiliangaleziowski@gmail.com>
2026-07-23 16:47:38 +0200

    feat: added Pagination

---
+++ ClinicManagement/ApplicationCore/Helpers/Pagination/PageResponse.cs
@@ -0,0 +1,12 @@
+namespace ApplicationCore.Helpers.Pagination;
+
+public class PageResponse<T>
+{
+    public IReadOnlyCollection<T> Data { get; init; } = [];
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public int TotalPages { get; init; }
+    public int TotalRecords { get; init; }
+    public bool HasNextPage => PageNumber < TotalPages;
+    public bool HasPreviousPage => PageNumber > 1;
+}
