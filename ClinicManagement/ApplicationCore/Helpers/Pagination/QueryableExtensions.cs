28cb1aec0a18d412d3dc9f76d7aee6762176dd04
maksymilian <maksymiliangaleziowski@gmail.com>
2026-07-23 16:47:03 +0200

    feat: added QueryableExtensions

---
+++ ClinicManagement/ApplicationCore/Helpers/Pagination/QueryableExtensions.cs
@@ -0,0 +1,11 @@
+namespace ApplicationCore.Helpers.Pagination;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<T> ApplyPagination<T>(this IQueryable<T> query, int pageNumber, int pageSize)
+    {
+        return query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize);
+    }
+}
