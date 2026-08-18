87a81e2294295ee5fa60c95b9ba6ad1d47640968
maksymilian <maksymiliangaleziowski@gmail.com>
2026-08-18 12:17:14 +0200

    feat: added Prescription details

--- ClinicManagement/ApplicationCore/Prescription/Dto/PrescriptionDto.cs
+++ ClinicManagement/ApplicationCore/Prescription/Dto/PrescriptionDto.cs
@@ -1,4 +1,7 @@
-﻿namespace ApplicationCore.Prescription.Dto;
+﻿using ApplicationCore.Medicine.Dto;
+using ApplicationCore.Patient.Dto;
+
+namespace ApplicationCore.Prescription.Dto;
 
 public class PrescriptionDto
 {
@@ -10,4 +13,11 @@ public class PrescriptionDto
         string Frequency,
         string Quantity,
         string Instructions);
+
+    public record PrescriptionDetails(
+        PatientDto.Response Patient,
+        DateTime Date,
+        string Code,
+        IEnumerable<MedicineDto.Details> MedicineDetails
+    );
 }
