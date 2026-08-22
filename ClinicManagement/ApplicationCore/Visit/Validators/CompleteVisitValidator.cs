55344affc01a129d9ebfec0f5b669ac2afbbf441
MAX <maksymiliangaleziowski@gmail.com>
2026-08-22 17:17:11 +0200

    feat: added CompleteVisitValidator

---
+++ ClinicManagement/ApplicationCore/Visit/Validators/CompleteVisitValidator.cs
@@ -0,0 +1,15 @@
+﻿using ApplicationCore.Visit.Dto;
+using FluentValidation;
+
+namespace ApplicationCore.Visit.Validators;
+
+public class CompleteVisitValidator : AbstractValidator<VisitDto.CreateDescription>
+{
+    public CompleteVisitValidator()
+    {
+        RuleFor(e => e.Description)
+            .NotEmpty().WithMessage("Description is required")
+            .NotNull().WithMessage("Description is required")
+            .MinimumLength(5).WithMessage("Description must be at least 5 characters long");
+    }
+}
